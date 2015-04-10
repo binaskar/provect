36a9dbc1965c18230a96e9caf788b74007ec93c0
abdulrahmanx <Abdulrahmanx.cs@gmail.com>
2015-04-10 17:31:44 +0300

    Arrhythmia Detection new C++ Implementation

---
+++ ArrhythmiaDetection/ArrhythmiaDetection/PwaveDetection.h
@@ -0,0 +1,14 @@
+//
+//  PwaveDetection.h
+//  ArrhythmiaDetection
+//
+//  Created by Abdulrahman on 4/4/15.
+//  Copyright (c) 2015 Abdulrahman. All rights reserved.
+//
+// Not Decided to be used yet
+#ifndef __ArrhythmiaDetection__PwaveDetection__
+#define __ArrhythmiaDetection__PwaveDetection__
+
+#include <stdio.h>
+
+#endif /* defined(__ArrhythmiaDetection__PwaveDetection__) */
