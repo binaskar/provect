c4ec261e1763634924d52f0178bd9a02872d75d9
abdulrahmanx <Abdulrahmanx.cs@gmail.com>
2015-04-18 21:39:20 +0300

    Change Receiver

--- sensor/sensor/test1.php
+++ sensor/sensor/test1.php
@@ -2,8 +2,22 @@
  
 if( isset($_POST["json"]) ) {
      $data = json_decode($_POST["json"]);
-     $data->PatientName="111";
-	
-     echo json_encode($data);
+     //Files Required
+     //arrhthmyia Detector
+     //file Handling
+     //
+  
+     //$data->PatientName =;
+     $patientName = $data->PatientName;
+	 $data2 = array();
+	 foreach($data->sTime as $sTime && $data->id as $id 
+	 && $data->high2 as $high && $data->low2 as $low){
+	 	
+	 	
+		insertSatanard($sTime,$id,$high,$low);	
+		insertDB($sTime,$id,$high,$low);
+	 }
+	 $data->PatientName = "100";
+	 echo json_encode($data);
  
 }
